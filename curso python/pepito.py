122bef842be7c414dfdfed85b0fc5b89b861bbf1
Tenoch Ventura <tenochman@hotmail.com>
2022-01-29 17:26:01 -0800

    yo he comido tacos :)

---
+++ curso python/pepito.py
@@ -0,0 +1,24 @@
+
+linea = input()
+arreglo = linea.split()
+#arreglo [0] 1
+a = arreglo[0]
+b = arreglo[1]
+c = arreglo[2]
+d = arreglo[3]
+e = arreglo[4]
+
+a = int(a)
+b = int(b)
+c = int(c)
+d = int(d)
+e = int(e)
+
+suma = a + b + c + d + e
+ 
+promedio = suma/5
+
+promedio = int(promedio)
+
+
+print(promedio)
